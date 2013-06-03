e87c148490de3968112a9094cac69fccf6f2c053
wookiee <kevinlriedl@gmail.com>
2013-06-02 23:19:43 -0700

    changed the whizgraphics render testbench to something working

--- synthesis/peripherals/whizgraphics_render_tb.sv
+++ synthesis/peripherals/whizgraphics_render_tb.sv
@@ -1,7 +1,9 @@
 module tb_render();
+	import video_types::*;
+
 	localparam ACTIVE   = 1'b1;
 	localparam INACTIVE = 0'b0;
-	import video_types::*;
+
 	bit clk = 0;
    DataBus db(clk);
    logic drawline;
@@ -10,19 +12,16 @@ module tb_render();
 	whizgraphics DUT(.*, .db(db.peripheral));
 
 	initial forever #10 clk = ~clk;
-	initial begin
-		#10;
-		while (~renderComplete)begin
-			$display("renderComplete: %b", renderComplete);
-			drawline = ACTIVE;
-			#10;
-			drawline = INACTIVE;
-		end
 
-		$finish;
+	assign drawline = clk;
+
+	always @* begin
+		if(renderComplete)
+			$finish;
 	end
 
 	final begin
+		$display("renderComplete: %b", renderComplete);
 		$display("tb_render is done");
 	end
 
