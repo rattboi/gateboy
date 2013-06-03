968b7ce9387b149f7a54f8536f0b9d780c732dab
pjl <pjl@pdx.edu>
2013-06-03 00:52:14 -0700

    Added pgm output and test data

--- synthesis/peripherals/whizgraphics_render_tb.sv
+++ synthesis/peripherals/whizgraphics_render_tb.sv
@@ -11,13 +11,39 @@ module tb_render();
    Lcd lcd;
 	whizgraphics DUT(.*, .db(db.peripheral));
 
+    initial 
+    begin
+        //write data to tiles
+        DUT.tiles[0].rows[0][0] = '1;
+        DUT.tiles[0].rows[0][2] = '1;
+        DUT.tiles[0].rows[0][4] = '1;
+        DUT.tiles[0].rows[0][6] = '1;
+        DUT.tiles[0].rows[2][0] = '1;
+        DUT.tiles[0].rows[2][2] = '1;
+        DUT.tiles[0].rows[2][4] = '1;
+        DUT.tiles[0].rows[2][6] = '1;
+        DUT.tiles[0].rows[4][0] = '1;
+        DUT.tiles[0].rows[4][2] = '1;
+        DUT.tiles[0].rows[4][4] = '1;
+        DUT.tiles[0].rows[4][6] = '1;
+        DUT.tiles[0].rows[6][0] = '1;
+        DUT.tiles[0].rows[6][2] = '1;
+        DUT.tiles[0].rows[6][4] = '1;
+        DUT.tiles[0].rows[6][6] = '1;
+
+    end
+
+
 	initial forever #10 clk = ~clk;
 
 	assign drawline = clk;
 
 	always @* begin
 		if(renderComplete)
+        begin
+            writeLCD(lcd, "out.pgm");
 			$finish;
+        end
 	end
 
 	final begin
