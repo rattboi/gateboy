97091104eb617927d6194d581c0fceea4098a090
wookiee <kevinlriedl@gmail.com>
2013-06-03 16:16:12 -0700

    Fixed merge issue with render testbench

--- synthesis/peripherals/whizgraphics_render_tb.sv
+++ synthesis/peripherals/whizgraphics_render_tb.sv
@@ -14,23 +14,27 @@ module tb_render();
     initial 
     begin
         //write data to tiles
-        DUT.tiles.Data[0].rows[0][0] = '1;
-        DUT.tiles.Data[0].rows[0][2] = '1;
-        DUT.tiles.Data[0].rows[0][4] = '1;
-        DUT.tiles.Data[0].rows[0][6] = '1;
-        DUT.tiles.Data[0].rows[2][0] = '1;
-        DUT.tiles.Data[0].rows[2][2] = '1;
-        DUT.tiles.Data[0].rows[2][4] = '1;
-        DUT.tiles.Data[0].rows[2][6] = '1;
-        DUT.tiles.Data[0].rows[4][0] = '1;
-        DUT.tiles.Data[0].rows[4][2] = '1;
-        DUT.tiles.Data[0].rows[4][4] = '1;
-        DUT.tiles.Data[0].rows[4][6] = '1;
-        DUT.tiles.Data[0].rows[6][0] = '1;
-        DUT.tiles.Data[0].rows[6][2] = '1;
-        DUT.tiles.Data[0].rows[6][4] = '1;
-        DUT.tiles.Data[0].rows[6][6] = '1;
+        //DUT.tiles[0].rows[0][0] = '1;
+        //DUT.tiles[0].rows[0][2] = '1;
+        //DUT.tiles[0].rows[0][4] = '1;
+        //DUT.tiles[0].rows[0][6] = '1;
+        //DUT.tiles[0].rows[2][0] = '1;
+        //DUT.tiles[0].rows[2][2] = '1;
+        //DUT.tiles[0].rows[2][4] = '1;
+        //DUT.tiles[0].rows[2][6] = '1;
+        //DUT.tiles[0].rows[4][0] = '1;
+        //DUT.tiles[0].rows[4][2] = '1;
+        //DUT.tiles[0].rows[4][4] = '1;
+        //DUT.tiles[0].rows[4][6] = '1;
+        //DUT.tiles[0].rows[6][0] = '1;
+        //DUT.tiles[0].rows[6][2] = '1;
+        //DUT.tiles[0].rows[6][4] = '1;
+        //DUT.tiles[0].rows[6][6] = '1;
 
+		  for(int i=0;i <= 255;i++)
+		  begin
+		     DUT.tiles[0].raw[i] = '1;
+		  end
     end
 
 
