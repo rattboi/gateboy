e2285179983f5dabccfa48a0c2cbc11e264eea7f
Andy Goetz <andy@andygoetz.org>
2013-06-02 19:34:12 -0700

    Fixed makefile

--- synthesis/peripherals/runsim.sh
+++ synthesis/peripherals/runsim.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 export PATH=/pkgs/mentor/questa/current/questasim/bin:$PATH
 MODULE=pgm_tb
+
+if [ -n "$1" ] ; then
+    MODULE=$1
+fi
+echo Testing module $MODULE
 FILES='whizzgraphics_tb.sv video_types.sv whizgraphics.sv ../interconnect/data_bus.inf pgm_tb.sv'
 for i in "$FILES" 
 do
