c71e3d239076826b22ba6654dea0c85210214b85
pjl <pjl@pdx.edu>
2013-06-02 22:56:50 -0700

    Fixed some array ordering

--- synthesis/peripherals/video_types.sv
+++ synthesis/peripherals/video_types.sv
@@ -76,23 +76,8 @@ package video_types;
 
     typedef union packed
     {
-        bit [0:7] raw;
-        struct packed
-        {
-            bit BgOamPriority;
-            bit VerticalFlip;
-            bit HorizontalFlip;
-            bit Unused;
-            bit TileBankNumber;
-            bit [0:2] BackgroundPallete;
-        } Fields;
-    } BackgroundMapAttrs;
-
-    typedef struct packed
-    {
         bit [0:(32*32)-1][0:7] Bits;
-        bit [0:7][0:31][0:31] BackgroundMap;        
-        BackgroundMapAttrs Attributes;
+        bit [0:31][0:31][0:7] BackgroundMap;        
     } vram_background;
 
     typedef union packed
