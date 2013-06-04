538f3b1a03004f5111b5f4d153b9aa9914baa753
apropos <ttricker@pdx.edu>
2013-06-03 17:25:07 -0700

    - fixed build issues caused by a type mismatch

--- synthesis/testbenches/tv80_alu_tb.sv
+++ synthesis/testbenches/tv80_alu_tb.sv
@@ -27,25 +27,6 @@ tv80_alu alu(
   Q, F_Out,  // Outputs
   Arith16, Z16, ALU_Op, IR, ISet, BusA, BusB, F_In); // Inputs
 
-// This enum is order
-typedef enum bit[3:0] {
-    ADD, //0000
-    ADC, //0001
-    SUB, //0010
-    SDC, //0011
-    AND, //0100
-    XOR, //0101
-    OR,  //0110
-    CP,  //0111
-    ROT, //1000
-    BIT, //1001
-    SET, //1010
-    RES, //1011
-    DAA, //1100
-    RLD, //1101
-    RRD //1110
-} alu_operation;
-
 
 always begin 
     $display("Testing things and stuff");
