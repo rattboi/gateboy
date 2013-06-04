08a30d4a620d176e1425460623e9028ef94901d2
Andy Goetz <andy@andygoetz.org>
2013-06-03 18:12:32 -0700

    Fixed basic whizgraphics testbench to prevent it from shitting all over the output

--- synthesis/peripherals/whizzgraphics_tb.sv
+++ synthesis/peripherals/whizzgraphics_tb.sv
@@ -10,26 +10,29 @@ module w_tb();
    bit [db.DATA_SIZE-1:0] r;
    bit [db.ADDR_SIZE-1:0] address;
    int        numPassed = 0;
+   int        numFailed = 0;
    bit [db.DATA_SIZE-1:0] d;
    
    initial forever #10 clk = ~clk;
    
    initial begin
 
-      for (int i = 0; i < 900; i++) begin
+      for (int i = 0; i < 900; i++) begin : stop
+         if (i >= DUT.OAM_SIZE)
+           disable stop;
          r = $urandom;
          address = (i & DUT.OAM_MASK) + DUT.OAM_LOC;
          db.write(r, address);
          db.read(address,d);
-         if (d != r)
-           $display("Could not transfer byte %x to addr %x", r, address);
-         else begin
-            $display("read %x from addr %x", d, address);
+         if (d == r) 
             numPassed++;
-         end
+         else
+           numFailed++;
+
       end
       
       $display("Passed %d tests", numPassed);
+      $display("Failed %d tests", numFailed);
       $finish;
    end
 endmodule
