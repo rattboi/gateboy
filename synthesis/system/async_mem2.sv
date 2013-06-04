386a1704fdcf21ea9a9ef0a2cc83bce37b5d27a0
apropos <ttricker@pdx.edu>
2013-06-03 19:36:10 -0700

    - change async_mem2 to k&r style declaration

--- synthesis/system/async_mem2.sv
+++ synthesis/system/async_mem2.sv
@@ -1,22 +1,33 @@
-`default_nettype none
 `timescale 1ns / 1ps
 
 module async_mem2(
-  input  wire           clkA,
-  input  wire           clkB,
-  input  wire [asz-1:0] addrA,
-  input  wire [asz-1:0] addrB,
-  input  wire           wr_csA,
-  input  wire           wr_csB,
-  input  wire     [7:0] wr_dataA,
-  input  wire     [7:0] wr_dataB,
-  output wire     [7:0] rd_dataA,
-  output wire     [7:0] rd_dataB
+  clkA,
+  clkB,
+  addrA,
+  addrB,
+  wr_csA,
+  wr_csB,
+  wr_dataA,
+  wr_dataB,
+  rd_dataA,
+  rd_dataB
 );
 
   parameter asz = 15;
   parameter depth = 32768;
-  
+
+  input  wire           clkA;
+  input  wire           clkB;
+  input  wire [asz-1:0] addrA;
+  input  wire [asz-1:0] addrB;
+  input  wire           wr_csA;
+  input  wire           wr_csB;
+  input  wire     [7:0] wr_dataA;
+  input  wire     [7:0] wr_dataB;
+
+  output wire     [7:0] rd_dataA;
+  output wire     [7:0] rd_dataB;
+
   reg [7:0] mem [0:depth-1];
   
   always @(posedge clkA) begin
