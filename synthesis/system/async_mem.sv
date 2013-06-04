211d38e99bf0b31968550be5e7c71f0e54a9db22
rattboi <rattboi@gmail.com>
2013-06-03 21:10:36 -0700

    Cleaned up async_mem.sv

--- synthesis/system/async_mem.sv
+++ synthesis/system/async_mem.sv
@@ -1,29 +1,29 @@
 module async_mem (/*AUTOARG*/
   // Outputs
-  rd_data, 
+  rd_data,
   // Inputs
   wr_clk, wr_data, wr_cs, addr, rd_cs
   );
 
   parameter asz = 15;
   parameter depth = 32768;
-  
-  input       wr_clk;
-  input [7:0] wr_data;
-  input       wr_cs;
+
+  input           wr_clk;
+  input [7:0]     wr_data;
+  input           wr_cs;
 
   input [asz-1:0] addr;
-  inout [7:0] 	  rd_data;
-  input 	  rd_cs;
+  inout [7:0]     rd_data;
+  input           rd_cs;
 
-  reg [7:0] 	  mem [0:depth-1];
+  logic [7:0]     mem [0:depth-1];
 
   always @(posedge wr_clk)
     begin
       if (wr_cs)
-	mem[addr] <= #1 wr_data;
+        mem[addr] <= #1 wr_data;
     end
 
-  assign rd_data = (rd_cs) ? mem[addr] : {8{1'bz}};
-	  
+  assign rd_data = (rd_cs) ? mem[addr] : 'z;
+
 endmodule // async_mem
