87e7080cd347495353d2e625a0939334953182a0
Andy Goetz <andy@andygoetz.org>
2013-06-04 22:24:04 -0700

    Added testrunner, and made it use the whizzgraphics test. However, memory bus is not working right now...

---
+++ synthesis/peripherals/test_runner.sv
@@ -0,0 +1,30 @@
+// implements a runner for a bunch of test objects
+
+module TestRunner();
+   import Test::*;
+   import video_types::*;
+   BaseTest tests[$];
+   bit clk = 0;
+   DataBus db(clk);
+   Control cntrl(clk);
+   whizgraphics #(.DEBUG_OUT(0)) DUT(.db(db.peripheral), .cntrl(cntrl.DUT));
+   initial forever #10 clk = ~clk;
+   initial begin
+      automatic  whizzgraphics wg = new;
+      tests.push_front(wg);
+
+      while (tests.size() > 0) begin
+         int numPassed, numFailed;
+         BaseTest t;
+         t = tests.pop_front();
+         t.db = db;
+         t.cntrl = cntrl;
+         $display("Testing %m", t);
+         t.runTest(numPassed, numFailed);
+         $display("Passed Tests: %0d", numPassed);
+         $display("Failed Tests: %0d", numFailed);
+         $finish;
+      end
+      
+   end
+endmodule
